7fc8e11c7109243d1e7a1a0a4e6b9fe591ef4255
Ryouji <1354413904@qq.com>
2022-03-13 12:11:20 +0800

    feat(layout): 实现了layout组件的基本结构以及样式

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>layout</div>
+  <div class="app-wrapper">
+    <side-bar class="sidebar-container" />
+    <div class="main-container">
+      <div class="fixed-header">
+        <nav-bar />
+      </div>
+      <app-main></app-main>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import NavBar from './components/NavBar'
+import SideBar from './components/SideBar'
+import AppMain from './components/AppMain'
 </script>
 
 <style lang="scss" scoped>
+@import "~@/styles/mixin.scss";
+@import "~@/styles/variables.scss";
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
-// 导入 svgIcon
 import installIcons from '@/icons'
 import './permission'
 
